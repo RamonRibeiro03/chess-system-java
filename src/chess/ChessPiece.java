1d36cada7a90bbdc6836c2c54c82feaabf8fd68a
Ramon Ribeiro <ramon.procisa@gmail.com>
2023-06-24 01:05:07 -0300

    Piece move count

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -18,6 +18,17 @@ public abstract class ChessPiece extends Piece {
 
         return color;
     }
+    public int getMoveCount(){
+        return moveCount;
+    }
+
+    public void increaseMoveCount(){
+        moveCount ++;
+    }
+    public void decreaseMoveCount(){
+        moveCount --;
+    }
+
 
     public ChessPosition getChessPosition(){
         return ChessPosition.fromPosition(position);
